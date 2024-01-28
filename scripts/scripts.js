46bf3e7d386ff074d75e06ab7aeb47ef38a3ee7b
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-01-28 19:57:41 -0300

    Add mouseover event

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,12 +3,15 @@ console.log("Good day sir");
 const squareGrid = document.querySelector(".square-grid");
 
 for (let i = 0; i < 16; i++) {
-    const divRow = document.createElement("div");
+    const divColumn = document.createElement("div");
+    divColumn.className = "square-column";
     for (let j = 0; j < 16; j++) {
         const div = document.createElement("div");
-        div.textContent = j;
         div.className = "singular-square";
-        divRow.appendChild(div);
+        div.addEventListener('mouseover', () => {
+            div.style.cssText = 'background-color:black;';
+        })
+        divColumn.appendChild(div);
     }
-    squareGrid.appendChild(divRow);
+    squareGrid.appendChild(divColumn);
 }
