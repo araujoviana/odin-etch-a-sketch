555a9cc18c98cb44fff73bb6f222b488e5195ed8
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-01-28 19:18:48 -0300

    Fix grid iterator name

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,10 +4,11 @@ const squareGrid = document.querySelector(".square-grid");
 
 for (let i = 0; i < 16; i++) {
     const divRow = document.createElement("div");
-    for (let j = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
         const div = document.createElement("div");
-        div.textContent = "test";
+        div.textContent = j;
         divRow.appendChild(div);
     }
     squareGrid.appendChild(divRow);
 }
+alert(squareGrid);
